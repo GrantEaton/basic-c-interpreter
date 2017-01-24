fa50332a667ce557122eaab7213aa7f1ebcda819
Grant Eaton <grantman100@gmail.com>
2017-01-24 01:52:22 -0500

    parse lines not tokens.

--- interpreter.cpp
+++ interpreter.cpp
@@ -1,23 +1,35 @@
 #include <iostream>
 #include <fstream>
+#include <map>
+#include <string>
 using namespace std;
 
 
 int main() {
-cout << "hello";
 string line;
 ifstream programFile ("TesterPrograms/prog1.zpm");
 
 	if (programFile.is_open()){
 		while(! programFile.eof()){
-            		programFile >> line;
-            		cout << line << endl;
+            		getline(programFile, line);
+            		//cout << line << endl;
+					for(int j=0; j<line.length();j++){
+
+
+
+
+
+
+
+					}
         	}
 	 	programFile.close();
 
 	 }
-	else cout << "Unable to open file. Check you have the right file name.";
+	else cout << "Unable to open file. Check its right file name.";
 
 	return 0;
 }
 
+	
+	
