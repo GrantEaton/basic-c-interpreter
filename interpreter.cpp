98a696aff67da36b98290e76b3088a22dc287e11
Grant Eaton <grantman100@gmail.com>
2017-01-29 20:16:18 -0500

    added A = B functionality

--- interpreter.cpp
+++ interpreter.cpp
@@ -76,11 +76,28 @@ vector<string> tokenize(string line){
 	vector<string> lineList = split(line, ' '); 	
 	return lineList;
 }
+//check if string is in vars map
+bool isVariable(string var, map<string,anyType> *vars){
+	if(vars->count(var) > 0){
+		return true;
+	}
+	return false;
+
+}
+//check if string is a reserved string
+bool isReservedString(string reservedString, map<string,bool> *reservedStrings){
+	if(reservedStrings->count(reservedString) > 0){
+		return true;
+	}
+	return false;
+
+}
+
 int main() {
 	string a = "hello my name is  x = i ;";
 	string line;
 	ifstream programFile ("TesterPrograms/prog1.zpm");
-	map<string,anyType> values;
+	map<string,anyType> vars;
 	map<string, bool> reservedStrings;
 	initializeReservedStrings(&reservedStrings);
 	string b = "hi";
@@ -90,7 +107,7 @@ int main() {
 
 	if (programFile.is_open()){
 		while(! programFile.eof()){
-		lineCount++;
+			lineCount++;
 			getline(programFile, line);
 			//cout << line << endl;
 			vector<string> tokens = split(line, ' ');
@@ -98,14 +115,26 @@ int main() {
 				return 1;
 			}	
 			for(int j=0; j<tokens.size();j++){					
-				if(reservedStrings.count(tokens[j]) > 0){
+				if(isReservedString(tokens[j], &reservedStrings)){
 					// do reserved strings stuff
 				}
-				else if(values.count(tokens[j]) > 0){
+				else if(vars.count(tokens[j]) > 0){
 					if(tokens[j+1] == "="){
 						anyType newVal;
+						locale loc("en-US");
+						//if first value of next token is a previous value
+						//ex A = B
+						if(isVariable(tokens[j+2],&vars)){
+							//assign B's val to A
+							anyType val = vars[tokens[j+2]];
+							vars.insert(pair<string,anyType>(tokens[j+2], val));
+						}
+						else if(isalpha(tokens[j+2].at(0),loc)){ 
 
-						values.insert(pair<string,anyType>(tokens[j],newVal));	
+							
+						}
+
+						vars.insert(pair<string,anyType>(tokens[j],newVal));	
 					}
 				}
 
